c83b263187f4dfb2b218136fa86a3f510d0e0bd8
wooil <boxer1532@ajou.ac.kr>
2020-11-24 12:33:17 +0900

    Modify time information

--- CS473_BFFT/js/time.js
+++ CS473_BFFT/js/time.js
@@ -1,8 +1,24 @@
 var d = new Date();
 var year = d.getFullYear().toString();
+
 var month = (d.getMonth() + 1).toString();
+if(parseInt(month) < 10)
+    month = '0' + month;
+
 var day = d.getDate().toString();
+if(parseInt(day) < 10)
+    day = '0' + day;
+
 var hours = d.getHours().toString();
+if(parseInt(hours) < 10)
+    hours = '0' + hours;
+
 var min = d.getMinutes().toString();
+if(parseInt(min) < 10)
+    min = '0' + min;
+
 var sec = d.getSeconds().toString();
+if(parseInt(sec) < 10)
+    sec = '0' + sec;
+
 var currentTime = year + month + day + hours + min + sec;
