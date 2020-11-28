8d552867ceb7fe65fd5b3ac6539d291ba8bcfe72
MoonlightRiver <chltmdgh16@kaist.ac.kr>
2020-11-28 18:23:40 +0900

    added one line

--- CS473_BFFT/js/chat.js
+++ CS473_BFFT/js/chat.js
@@ -173,3 +173,11 @@ function loadMessages() {
         updateScrollbar();
     });
 }
+
+function doNotReload(){
+    if( (event.ctrlKey == true && (event.keyCode == 78 || event.keyCode == 82)) || (event.keyCode == 116) ) {
+        event.keyCode = 0;
+        event.cancelBubble = true;
+        event.returnValue = false;
+    } 
+}
